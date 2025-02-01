adfad671a92a94a7790f11afc5ce7bf5d4aa9eca
NevMan1 <97640974+NevMan1@users.noreply.github.com>
2025-01-31 20:37:01 -0800

    Update server_c_tcp.c

--- server_c_tcp.c
+++ server_c_tcp.c
@@ -115,7 +115,7 @@ int main(int argc, char *argv[])
             bzero(result_buffer, 256); // Clear the result buffer
         } else {
             // If not valid input, send error message
-            read_bytes = write(client_fd, "Sorry, cannot compute!\n", 23);
+            read_bytes = write(client_fd, "Sorry, cannot compute\n", 23);
             if (read_bytes < 0) {
                 handle_error("ERROR writing to socket");
             }
